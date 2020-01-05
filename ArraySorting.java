d5e4402889ada741dee3456d6e8016f5a524fd4d
Chris Ingham <inghamchristopher7@gmail.com>
2020-01-04 19:46:21 -0500

    Add files via upload

---
+++ ArraySorting.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArraySorting {
+
+    public static void main (String[] args){
+        int [] grades = {1, 2, 3, 72, 5};
+
+        Arrays.parallelSort(grades);
+        System.out.println(Arrays.toString(grades));
+    }
+
+
+
+
+
+
+}
---
+++ StringsArraysNArrayLists.java
@@ -0,0 +1,52 @@
+/*
+Strings 
+Arrays
+ArrayLists
+
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringsArraysNArrayLists{
+
+    /* USING A SCANNER TO ASK A USER FOR LENGTH OF ARRAY THEY
+    WANT TO MAKE AND THEN THEM ADDING THE VALUES 
+    
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+
+        int[] grades = new int[size];
+
+      
+
+        for(int i=0; i<size; i++){
+            int x = input.nextInt();
+            grades[i] = x;
+            
+        }
+        
+        
+        
+        System.out.println(Arrays.toString(grades));
+
+    }
+    */
+    /* Searching an array for a value at an index
+    public static void main(String[] args){
+        int[] grades  = {1,2,72,30};      
+        
+        for(int i=0; i<grades.length;i++){
+            if (grades[i] == 72){
+
+                System.out.println("Found at index: " + i);
+            }
+            
+        }
+    }
+    */ 
+    
+
+
+}
